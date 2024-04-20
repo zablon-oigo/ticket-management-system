60022c7ccb8539a6da1202815a0590902370036a
zablon <zablonoigo51@gmail.com>
2024-04-20 12:00:36 +0300

    Custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    is_customer=models.BooleanField(default=False)
+    is_engineer=models.BooleanField(default=False)
+    email=models.EmailField(unique=True)
