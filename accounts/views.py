2225cd5480e2d7dd5b4c5ac82ef2486a73ef65d1
zablon <zablonoigo51@gmail.com>
2024-04-20 12:12:16 +0300

    Register customer view logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout, get_user_model
+from .forms import RegisterCustomerForm
 
-# Create your views here.
+User=get_user_model()
+
+def register_customer(request):
+    if request.method =='POST':
+        form=RegisterCustomerForm(request.POST)
+        if form.is_valid():
+            cd=form.save(commit=False)
+            cd.is_customer=True
+            cd.save()
+            messages.success(request,'Account created. Please log in')
+            return redirect('login')
+        else:
+            messages.error(request,"Please correct the Error .")
+            return redirect("register-customer")
+    else:
+        form=RegisterCustomerForm()
+        return render(request,'accounts/register.html',{'form':form})
