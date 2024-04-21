5d48c3eb166c4f2a6610354ee4bab16774d6ceec
zablon <zablonoigo51@gmail.com>
2024-04-21 12:32:18 +0300

    Adding string method

--- accounts/models.py
+++ accounts/models.py
@@ -5,3 +5,9 @@ class CustomUser(AbstractUser):
     is_customer=models.BooleanField(default=False)
     is_engineer=models.BooleanField(default=False)
     email=models.EmailField(unique=True)
+
+    class Meta:
+        ordering=['-email']
+    
+    def __str__(self):
+        return f'{self.email.lower()}'
