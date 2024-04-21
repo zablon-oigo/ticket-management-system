bc93d110f703bdcf1728373e9db73e7131f839b7
zablon <zablonoigo51@gmail.com>
2024-04-21 11:20:58 +0300

    Adding dashboard view function

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def dashboard(request):
+    if request.user.is_customer:
+        return render(request,'dashboard/customer_dashboard.html')
