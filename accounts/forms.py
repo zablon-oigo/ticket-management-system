0148b97eae756610d64ff1a9ee3f2dc648b92c08
zablon <zablonoigo51@gmail.com>
2024-04-21 13:19:15 +0300

    Correcting list values on form

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,5 +5,5 @@ User=get_user_model()
 class RegisterCustomerForm(UserCreationForm):
     class Meta:
         model=User
-        fields=['email','paasword1','password2']
+        fields=['email','password1','password2']
 
