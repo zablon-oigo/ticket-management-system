ef8660ca2d513c202fa8b9fea5f3bc21ae6c7986
zablon <zablonoigo51@gmail.com>
2024-04-21 13:20:40 +0300

    Adding login decorator

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+@login_required
 def dashboard(request):
     if request.user.is_customer:
         return render(request,'dashboard/customer_dashboard.html')
