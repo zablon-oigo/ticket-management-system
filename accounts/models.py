7f0424116aa474af2b1e36a5cdb9d890f6d4a245
zablon <zablonoigo51@gmail.com>
2024-04-22 11:21:37 +0300

    Override Profile model with a save method

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from PIL import Image
 class CustomUser(AbstractUser):
     is_customer=models.BooleanField(default=False)
     is_engineer=models.BooleanField(default=False)
@@ -23,3 +24,11 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'Profile of {self.user.email}'
+    
+    def save(self,*args,**kwargs):
+        super().save(*args,**kwargs)
+        img=Image.open(self.photo.path)
+        if img.height > 300 or img.width >300 :
+            output_size=(300,200)
+            img.thumbnail(output_size)
+            img.save(self.photo.path)
