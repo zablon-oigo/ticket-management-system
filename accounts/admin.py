ad20d19a24f0fac1071af4a51117e53c8fc999fc
zablon <zablonoigo51@gmail.com>
2024-04-22 11:33:43 +0300

    Adding Profile model to the admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomUser,Profile
 
-# Register your models here.
+@admin.site(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=['user','photo','date_joined']
+    list_filter=['date_joined']
