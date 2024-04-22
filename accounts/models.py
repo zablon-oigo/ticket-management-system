c2ea0cf057d1debad0156c0c3e2d97f36c7a6976
zablon <zablonoigo51@gmail.com>
2024-04-22 11:18:23 +0300

    Creating users profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.utils import timezone
 class CustomUser(AbstractUser):
     is_customer=models.BooleanField(default=False)
     is_engineer=models.BooleanField(default=False)
@@ -11,3 +11,15 @@ class CustomUser(AbstractUser):
     
     def __str__(self):
         return f'{self.email.lower()}'
+    
+
+class Profile(models.Model):
+    user=models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    photo=models.ImageField(upload_to='profile/%Y', default='avatar.jpg')
+    joined_date=models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering=['-user']
+    
+    def __str__(self):
+        return f'Profile of {self.user.email}'
