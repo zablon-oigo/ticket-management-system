50cda20605abb9a7c93d75d9d1af2a4490652e04
zablon <zablonoigo51@gmail.com>
2024-04-22 11:35:44 +0300

    Creating a custom user form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth import get_user_model
 User=get_user_model()
 
@@ -7,3 +8,15 @@ class RegisterCustomerForm(UserCreationForm):
         model=User
         fields=['email','password1','password2']
 
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ("email",)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ("email",)
