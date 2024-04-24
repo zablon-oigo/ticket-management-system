a584913cb5ad6702535985ad3d4962a8adcfdd18
zablon <zablonoigo51@gmail.com>
2024-04-24 10:17:51 +0300

    Ticket assigning form for engineers

--- ticket/forms.py
+++ ticket/forms.py
@@ -5,3 +5,9 @@ class CreateTicketForm(forms.ModelForm):
     class Meta:
         model=Ticket
         fields=['ticket_title','ticket_description','contact_mode']
+
+
+class AssignTicketForm(forms.ModelForm):
+    class Meta:
+        model=Ticket
+        fields=['engineer']
