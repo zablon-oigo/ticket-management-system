f0951ea54bbf45c745673756a5568653df675751
zablon <zablonoigo51@gmail.com>
2024-04-24 09:57:02 +0300

    Ticket ModelForm

--- ticket/forms.py
+++ ticket/forms.py
@@ -1,2 +1,7 @@
 from django import forms
 from .models import Ticket
+
+class CreateTicketForm(forms.ModelForm):
+    class Meta:
+        model=Ticket
+        fields=['ticket_title','ticket_description','contact_mode']
