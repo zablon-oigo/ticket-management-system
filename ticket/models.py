2669942616f211fe113dc3ac51a495aff5f48e73
zablon <zablonoigo51@gmail.com>
2024-04-24 09:53:24 +0300

    New ticket model

--- ticket/models.py
+++ ticket/models.py
@@ -4,5 +4,17 @@ from django.contrib.auth import get_user_model
 from django.utils import timezone
 User=get_user_model()
 
-
+class Ticket(models.Model):
+    customer=models.ForeignKey(User, on_delete=models.CASCADE)
+    engineer=models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='engineer')
+    ticket_id=models.CharField(max_length=15, unique=True)
+    ticket_title=models.CharField(max_length=50)
+    ticket_description=models.TextField()
+    status=models.CharField(max_length=20, choices=(('Active','Active'),('Pending','Pending'),('Resolved','Resolved')),default='Pending')
+    created_on=models.DateTimeField(auto_now_add=True)
+    last_modified=models.DateTimeField(auto_now=True)
+    is_resolved=models.BooleanField(default=False)
+    severity=models.CharField(max_length=5,choices=(('A','A'),('B','B')), default='B')
+    contact_mode=models.CharField(max_length=20, choices=(('Phone','Phone'),('Email','Email')))
+    is_assigned_to_engineer=models.BooleanField(default=False)
 
