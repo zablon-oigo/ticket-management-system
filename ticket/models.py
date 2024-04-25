0423e70a6ac58ce06470358f68ac560f7672565c
zablon <zablonoigo51@gmail.com>
2024-04-25 10:40:02 +0300

    Adding null and balck argument

--- ticket/models.py
+++ ticket/models.py
@@ -6,7 +6,7 @@ User=get_user_model()
 
 class Ticket(models.Model):
     customer=models.ForeignKey(User, on_delete=models.CASCADE)
-    engineer=models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='engineer')
+    engineer=models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='engineer', null=True, blank=True)
     ticket_id=models.CharField(max_length=15, unique=True)
     ticket_title=models.CharField(max_length=50)
     ticket_description=models.TextField()
