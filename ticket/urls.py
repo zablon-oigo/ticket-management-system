bdf1174dca755f789026c523748fb94a74eb84e5
zablon <zablonoigo51@gmail.com>
2024-04-25 10:25:47 +0300

    Ticket urls patterns

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
-from .views import (ticket_details,ticket_queue,create_ticket)
+from .views import (ticket_details,ticket_queue,create_ticket,assign_ticket ,customer_tickets)
 
+urlpatterns=[
+    path('create-ticket/', create_ticket, name='create_ticket'),
+    path('customer-tickets/', customer_tickets, name='customer-tickets'),
+    path('assign-ticket/<str:ticket_id>/', assign_ticket, name='assign-ticket'),
+    path('ticket-details/<str:ticket_id>/', ticket_details, name='ticket-details'),
+    path('ticket-queue/', ticket_queue, name='ticket-queue'),
+]
