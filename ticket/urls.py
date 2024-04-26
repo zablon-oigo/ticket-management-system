3634e284106a2258b43f340d1f8fe3b871508671
zablon <zablonoigo51@gmail.com>
2024-04-26 13:09:34 +0300

    Adding unresolved ticket path

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import (ticket_details,ticket_queue,create_ticket,assign_ticket ,customer_tickets)
+from .views import (ticket_details,ticket_queue,create_ticket,assign_ticket ,customer_active_tickets,customer_resolved_tickets)
 
 urlpatterns=[
     path('create-ticket/', create_ticket, name='create_ticket'),
-    path('customer-tickets/', customer_tickets, name='customer-tickets'),
+    path('customer-tickets/', customer_active_tickets, name='customer-tickets'),
+    path('customer-resolved-tickets/', customer_resolved_tickets, name='resolved'),
     path('assign-ticket/<str:ticket_id>/', assign_ticket, name='assign-ticket'),
     path('ticket-details/<str:ticket_id>/', ticket_details, name='ticket-details'),
     path('ticket-queue/', ticket_queue, name='ticket-queue'),
