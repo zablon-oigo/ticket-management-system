c1721856b1295b41760506b330f65119f06cc495
zablon <zablonoigo51@gmail.com>
2024-04-26 13:08:57 +0300

    Renamed the customer view function

--- ticket/views.py
+++ ticket/views.py
@@ -31,7 +31,7 @@ def create_ticket(request):
         context={'form':form}
         return render(request, 'ticket/create_ticket.html',context)
     
-def customer_tickets(request):
+def customer_active_tickets(request):
     tickets=Ticket.objects.filter(customer=request.user, is_resolved=False).order_by('created_on')
     context={'tickets',tickets}
     return render(request, 'ticket/customer_tickets.html',context)
