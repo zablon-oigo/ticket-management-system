3bf3ce1795b34505f6cd2e8fc0043add26ffcdf7
zablon <zablonoigo51@gmail.com>
2024-04-27 11:28:53 +0300

    Adding engineer dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,3 +4,5 @@ from django.contrib.auth.decorators import login_required
 def dashboard(request):
     if request.user.is_customer:
         return render(request,'dashboard/customer_dashboard.html')
+    elif request.user.is_engineer:
+        return render(request, 'dashboard/engineer_dashboard.html')
