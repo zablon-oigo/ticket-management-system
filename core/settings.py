d286c5286e763526fc9c492d6a5e56421475bb57
zablon <zablonoigo51@gmail.com>
2024-04-28 08:00:52 +0300

    Changing media path

--- core/settings.py
+++ core/settings.py
@@ -121,6 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL='media/'
-MEDIA_ROOT=BASE_DIR / "media"
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 AUTH_USER_MODEL='accounts.CustomUser'
