76fc89b817203fa3f84106b30916610872ccf74f
zablon <zablonoigo51@gmail.com>
2024-04-28 08:10:29 +0300

    Adding resolved and active ticket path

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import (ticket_details,ticket_queue,create_ticket,assign_ticket ,customer_active_tickets,customer_resolved_tickets)
+from .views import (ticket_details,ticket_queue,create_ticket,assign_ticket ,\
+                    customer_active_tickets,customer_resolved_tickets,engineer_active_tickets,engineer_resolved_tickets)
 
 urlpatterns=[
     path('create-ticket/', create_ticket, name='create_ticket'),
@@ -8,4 +9,6 @@ urlpatterns=[
     path('assign-ticket/<str:ticket_id>/', assign_ticket, name='assign-ticket'),
     path('ticket-details/<str:ticket_id>/', ticket_details, name='ticket-details'),
     path('ticket-queue/', ticket_queue, name='ticket-queue'),
+    path('engineer-active/', engineer_active_tickets, name='engineer-active'),
+    path('engineer-resolved/', engineer_resolved_tickets, name='engineer-resolved'),
 ]
