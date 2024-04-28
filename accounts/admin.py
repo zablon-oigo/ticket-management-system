17e67f6d4940f748c6a6f5958399250fafc11cda
zablon <zablonoigo51@gmail.com>
2024-04-28 08:01:22 +0300

    registering profile admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import CustomUser,Profile
 
-@admin.site(Profile)
+@admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display=['user','photo','date_joined']
     list_filter=['date_joined']
