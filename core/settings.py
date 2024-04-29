466fac732d76283669819fc36cd163fbf071319e
zablon <zablonoigo51@gmail.com>
2024-04-29 09:44:49 +0300

    Adding email configuration

--- core/settings.py
+++ core/settings.py
@@ -123,3 +123,4 @@ STATIC_URL = '/static/'
 MEDIA_URL='media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 AUTH_USER_MODEL='accounts.CustomUser'
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
