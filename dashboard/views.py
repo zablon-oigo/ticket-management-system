23e5222beb976a57a6cd41540d860bec38c7a184
zablon <zablonoigo51@gmail.com>
2024-04-29 10:06:59 +0300

    Filtering queryset for customer active ticket

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from ticket.models import Ticket
 @login_required
 def dashboard(request):
     if request.user.is_customer:
-        return render(request,'dashboard/customer_dashboard.html')
+        tickets=Ticket.objects.filter(customer=request.user)
+        active_tickets=Ticket.objects.filter(customer=request.user, is_resolved=False).count
+        closed_tickets=Ticket.objects.filter(customer=request.user, is_resolved=True).count
+        context={
+            'tickets':tickets,
+            'active_tickets':active_tickets,
+            'closed_tickets':closed_tickets,
+            }
+        return render(request,'dashboard/customer_dashboard.html',context)
     elif request.user.is_engineer:
         return render(request, 'dashboard/engineer_dashboard.html')
