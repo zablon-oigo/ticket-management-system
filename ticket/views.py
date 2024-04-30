5e1e7eda3c663198874853916e8efa03aa9197f9
zablon <zablonoigo51@gmail.com>
2024-04-30 14:11:46 +0300

    Adding resolved ticket view function

--- ticket/views.py
+++ ticket/views.py
@@ -89,3 +89,14 @@ def engineer_resolved_tickets(request):
     tickets=Ticket.objects.filter(engineer=request.user, is_resolved=True).order_by('-created_on')
     context={'tickets':tickets}
     return render(request, 'ticket/engineer_resolved_tickets.html', context)
+
+
+def resolve_ticket(request, ticket_id):
+    ticket=Ticket.objects.get(ticket_id=ticket_id)
+    if request.mthod == 'POST':
+        rs=request.POST.get('rs')
+        ticket.resolution_steps=rs
+        ticket.is_resolved=True
+        ticket.status='Resolved'
+        ticket.save()
+        messages.success(request,"Ticket is now resolved and closed")
