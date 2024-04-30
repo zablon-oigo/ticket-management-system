c1244bc5607f5d25c05e522ac97a5de3769d9067
zablon <zablonoigo51@gmail.com>
2024-04-30 14:01:51 +0300

    adding argument to resolution step

--- ticket/models.py
+++ ticket/models.py
@@ -16,5 +16,5 @@ class Ticket(models.Model):
     is_resolved=models.BooleanField(default=False)
     severity=models.CharField(max_length=5,choices=(('A','A'),('B','B')), default='B')
     is_assigned_to_engineer=models.BooleanField(default=False)
-    resolution_steps=models.TextField()
+    resolution_steps=models.TextField(null=True, blank=True)
 
