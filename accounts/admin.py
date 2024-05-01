b453f654ebf1702fe2b210421a0d9bdc0dd0c81a
zablon <zablonoigo51@gmail.com>
2024-05-01 11:31:56 +0300

    Adding list display and filter for customer model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import CustomUser,Profile
 
-admin.site.register(CustomUser)
-# admin.site.register(Profile)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display=['email','is_customer','is_engineer','is_active','is_admin','is_staff']
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display=['user','photo','joined_date']
