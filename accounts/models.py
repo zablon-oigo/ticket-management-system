63ec91e92d22be9fd16fe4fb3d4091759a8946c5
zablon <zablonoigo51@gmail.com>
2024-05-01 11:08:17 +0300

    Adding object manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
 from django.utils import timezone
 from PIL import Image
-class CustomUser(AbstractUser):
+from .managers import CustomUserManager
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email=models.EmailField(unique=True,verbose_name='Email Address', max_length=255)
     first_name=models.CharField(verbose_name='First Name', max_length=30,blank=True )
     last_name=models.CharField(verbose_name='Last Name', max_length=30,blank=True )
     is_customer=models.BooleanField(default=False)
     is_engineer=models.BooleanField(default=False)
-    email=models.EmailField(unique=True,verbose_name='Email Address', max_length=255)
     is_active=models.BooleanField(default=True)
     is_admin=models.BooleanField(default=False)
 
-    class Meta:
-        ordering=['-email']
+    objects=CustomUserManager()
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=[]
     
     def __str__(self):
         return f'{self.email.lower()}'
