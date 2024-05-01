f6c900caefdef24102949eac39fa35ad8b4302b6
zablon <zablonoigo51@gmail.com>
2024-05-01 11:45:56 +0300

    adding ordering clause for models

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,8 +5,10 @@ from .models import CustomUser,Profile
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display=['email','is_customer','is_engineer','is_active','is_admin','is_staff']
+    ordering=['email']
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display=['user','photo','joined_date']
     list_filter=['joined_date']
+    ordering=['joined_date']
