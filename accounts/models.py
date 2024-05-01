114a9e8e57d9117d54ab82cce00dc0757c200069
zablon <zablonoigo51@gmail.com>
2024-05-01 11:16:30 +0300

    Adding object permission

--- accounts/models.py
+++ accounts/models.py
@@ -19,6 +19,20 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return f'{self.email.lower()}'
     
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always 
+        return True
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always 
+        return True
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff 
+        return self.is_admin
+    
 
 class Profile(models.Model):
     user=models.OneToOneField(CustomUser, on_delete=models.CASCADE)
