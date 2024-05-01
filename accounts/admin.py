de550b16dfff92078aaea6e64a04e0c18c6c8c55
zablon <zablonoigo51@gmail.com>
2024-05-01 11:28:55 +0300

    Adding list display and filter

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import CustomUser,Profile
 
 admin.site.register(CustomUser)
-admin.site.register(Profile)
+# admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=['user','photo','joined_date']
+    list_filter=['joined_date']
