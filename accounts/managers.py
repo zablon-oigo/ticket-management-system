7aea0cf93bcd91dd5864a4d0077dfc7455f03cd7
zablon <zablonoigo51@gmail.com>
2024-05-02 12:10:30 +0300

    Removing extrafields on model manager

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,20 +1,22 @@
 from django.contrib.auth.base_user import BaseUserManager
 class CustomUserManager(BaseUserManager):
-    def create_user(self,email, password=None, **extra_fields):
+    def create_user(self,email, password, **extra_fields):
         if not email:
-            raise ValueError("Users must have an email address")
-        user=self.model(email=self.normalize_email(email),**extra_fields)
+            raise ValueError("Email address must be set")
+        email=self.normalize_email(email)
+        user=self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user
     
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_staff",True)
         extra_fields.setdefault("is_superuser",True)
         extra_fields.setdefault("is_active",True)
-        user=self.create_user(
-            email, password=password, **extra_fields
-        )
-        user.save(using=self._db)
-        return user
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Super user must have is_superuser=True")
+        return self.create_user(email, password, **extra_fields)
     
