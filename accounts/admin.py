772a77ae533d9e30d8b4d742b6cbace95d4d3bcd
zablon <zablonoigo51@gmail.com>
2024-05-02 11:26:14 +0300

    adding engineer and customer field

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser,Profile
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ("email", "is_staff", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active","groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active","is_engineer","is_customer","groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
 
 
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display=['email','is_customer','is_engineer','is_active','is_admin','is_staff']
-    ordering=['email']
+admin.site.register(CustomUser, CustomUserAdmin)
+# @admin.register(CustomUser)
+# class CustomUserAdmin(admin.ModelAdmin):
+#     list_display=['email','is_customer','is_engineer','is_active','is_admin','is_staff']
+#     ordering=['email']
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
