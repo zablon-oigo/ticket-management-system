1aa80785eab0425f89a8d98f2777f57f0cf9a429
zablon <zablonoigo51@gmail.com>
2024-05-02 13:52:57 +0300

    Override userform with a method to check user email

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from .models import CustomUser
+from django import forms
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth import get_user_model
 User=get_user_model()
@@ -7,7 +8,13 @@ class RegisterCustomerForm(UserCreationForm):
     class Meta:
         model=User
         fields=['email','password1','password2']
-
+    
+    def clean_email(self):
+        email=self.cleaned_data.get('email')
+        qs=User.objects.filter(email=email)
+        if qs.exists():
+            raise  forms.ValidationError("Email is already taken")
+        return email
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
