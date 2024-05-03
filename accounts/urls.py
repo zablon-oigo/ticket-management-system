61e986e071a833efdd54ed506d3aaaf6ae586a10
zablon <zablonoigo51@gmail.com>
2024-05-03 12:30:00 +0300

    Adding base url

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (register_customer,login_user,logout_user)
 
 urlpatterns=[
+    path('', login_user, name='login'),
     path('register-customer/', register_customer, name='register'),
-    path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
 ]
