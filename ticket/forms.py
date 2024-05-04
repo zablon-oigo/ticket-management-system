b4d7f9c64b8a1d03ab6b1acf41b1117922c2f8fe
zablon <zablonoigo51@gmail.com>
2024-05-04 12:46:16 +0300

    ticket create form

--- ticket/forms.py
+++ ticket/forms.py
@@ -4,7 +4,7 @@ from .models import Ticket
 class CreateTicketForm(forms.ModelForm):
     class Meta:
         model=Ticket
-        fields=['ticket_title','ticket_description','contact_mode']
+        fields=['ticket_title','ticket_description']
 
 
 class AssignTicketForm(forms.ModelForm):
