413fe029494ce5b945fbea56cd6822b830ed8701
zablon <zablonoigo51@gmail.com>
2024-05-06 11:19:16 +0300

    Adding form label

--- ticket/forms.py
+++ ticket/forms.py
@@ -5,15 +5,15 @@ class CreateTicketForm(forms.ModelForm):
     class Meta:
         model=Ticket
         fields=['ticket_title','ticket_description']
-    ticket_title=forms.CharField(widget=forms.TextInput(
+    ticket_title=forms.CharField( label='ticket title',widget=forms.TextInput(
             attrs={
                 'class':'px-6 py-4 w-full rounded-xl mb-2'
             }
 
     ))
-    ticket_description=forms.CharField(widget=forms.Textarea(
+    ticket_description=forms.CharField( label='ticket description',widget=forms.Textarea(
         attrs={
-                            'class':'px-6 py-4 w-full rounded-xl mb-2'
+            'class':'px-6 py-4 w-full rounded-xl mb-2'
         }
     ))
 
