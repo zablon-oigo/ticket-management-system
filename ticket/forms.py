74207b3b1069206c0a346aec261be2e62579e17e
zablon <zablonoigo51@gmail.com>
2024-05-06 11:08:27 +0300

    Styling Form Input

--- ticket/forms.py
+++ ticket/forms.py
@@ -5,6 +5,17 @@ class CreateTicketForm(forms.ModelForm):
     class Meta:
         model=Ticket
         fields=['ticket_title','ticket_description']
+    ticket_title=forms.CharField(widget=forms.TextInput(
+            attrs={
+                'class':'px-6 py-4 w-full rounded-xl mb-2'
+            }
+
+    ))
+    ticket_description=forms.CharField(widget=forms.Textarea(
+        attrs={
+                            'class':'px-6 py-4 w-full rounded-xl mb-2'
+        }
+    ))
 
 
 class AssignTicketForm(forms.ModelForm):
