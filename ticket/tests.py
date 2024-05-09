ca3d73cdcb1153de825deb3214675945c9f55803
zablon <zablonoigo51@gmail.com>
2024-05-09 11:58:36 +0300

    Testing ticket model

--- ticket/tests.py
+++ ticket/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Ticket
+from django.contrib.auth import get_user_model
+class TicketTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User = get_user_model()
+        cls.customer= User.objects.create_user(email="normal@user.com", password="secret")
+        cls.engineer= User.objects.create_user(email="engineer@user.com", password="secret")
+        cls.ticket=Ticket.objects.create(
+            ticket_title="ticket 1",
+            ticket_description="linux server installation",
+            is_resolved=False,
+            status="Pending",
+            customer=cls.customer,
+            engineer=cls.engineer
+        )
 
-# Create your tests here.
+    def test_model_content(self):
+        self.assertEqual(self.ticket.ticket_title,"ticket 1")
+        self.assertEqual(self.ticket.ticket_description,"linux server installation")
+        self.assertEqual(self.ticket.is_resolved,False)
+        self.assertEqual(Ticket.objects.count(),1)
+        
